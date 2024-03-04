9364382bccc4acdd4797b8bc8aa951942aa9f6a6
Isheanesu Zvikaramba <zvkish001@myuct.ac.za>
2024-03-04 02:47:04 +0200

    add styling to form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,68 +6,70 @@ export const meta: MetaFunction = () => [{ title: "Project 1: Home Town Schools"
 export default function Index() {
   return (
     <>
-      <div>
-        <h1>Add School:</h1>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="font-bold text-center">Add School</h1>
+        <Form className="space-y-6" action="/school" method="post">
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="name">School Name:</label>
+            <input required type="text" id="name" name="name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+          
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="seniorSchool">Senior School?</label>
+            <input type="checkbox"  id="seniorSchool"  name="seniorSchool"/>  
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900"  htmlFor="juniorSchool">Junior School?</label>
+            <input type="checkbox" id="juniorSchool" name="juniorSchool"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="emailDomain">Email Domain:</label>
+            <input type="text" name="emailDomain" id="emailDomain" defaultValue="" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <h3 className="font-semibold text-center">Address</h3>
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="nameOrNumber">Name or Number:</label>
+            <input required type="text" name="nameOrNumber" id="nameOrNumber" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="street">Street Line 1:</label>
+            <input type="text" name="street" id="street"  defaultValue="" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="street2">Street Line 2:</label>
+            <input type="text" name="street2" id="street" defaultValue="" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="townOrCity">Town/City:</label>
+            <input required type="text" name="townOrCity" id="townOrCity" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="region">Region:</label>
+            <input type="text" name="region" id="region" defaultValue="" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="addressCode">Address Code:</label>
+            <input required type="text" name="addressCode" id="addressCode" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="country">Country:</label>
+            <input required type="text" name="country" id="country" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+
+          <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Create School
+          </button>
+        </Form>
       </div>
-      <Form action="/school" method="post">
-        <div>
-          <label htmlFor="name">School Name:</label>
-          <input required type="text" id="name" name="name"/>
-        </div>
-        
-        <div>
-          <label htmlFor="seniorSchool">Senior School?</label>
-          <input type="checkbox"  id="seniorSchool"  name="seniorSchool"/>  
-        </div>
-
-        <div>
-          <label htmlFor="juniorSchool">Junior School?</label>
-          <input type="checkbox" id="juniorSchool" name="juniorSchool"/>
-        </div>
-
-        <div>
-          <label htmlFor="emailDomain">Email Domain:</label>
-          <input type="text" name="emailDomain" id="emailDomain" defaultValue="" />
-        </div>
-
-        <p>Address</p>
-        <div>
-          <label htmlFor="nameOrNumber">Name or Number</label>
-          <input required type="text" name="nameOrNumber" id="nameOrNumber" />
-        </div>
-
-        <div>
-          <label htmlFor="street">Street Line 1:</label>
-          <input type="text" name="street" id="street"  defaultValue=""/>
-        </div>
-
-        <div>
-          <label htmlFor="street2">Street Line 2:</label>
-          <input type="text" name="street2" id="street" defaultValue=""/>
-        </div>
-
-        <div>
-          <label htmlFor="townOrCity">Town/City:</label>
-          <input required type="text" name="townOrCity" id="townOrCity"/>
-        </div>
-
-        <div>
-          <label htmlFor="region">Region:</label>
-          <input type="text" name="region" id="region" defaultValue=""/>
-        </div>
-
-        <div>
-          <label htmlFor="addressCode">Address Code:</label>
-          <input required type="text" name="addressCode" id="addressCode" />
-        </div>
-
-        <div>
-          <label htmlFor="country">Country:</label>
-          <input required type="text" name="country" id="country" />
-        </div>
-
-        <button type="submit">Create School</button>
-      </Form>
     </>
   )
 }
